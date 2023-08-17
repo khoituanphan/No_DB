194127ae9265aaa81d533d833f1385995bb408e6
khoituanphan <tuankhoi.phan0820@gmail.com>
2023-08-17 11:09:23 +0700

    openaiupdate

--- pages/api/generateSum.js
+++ pages/api/generateSum.js
@@ -1,51 +1,63 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).end();
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const { chatId, numMessages, projectID, userName, userSecret } = req.body;
+  console.log("Received data:", req.body);
+
+  if (!chatId || !numMessages) {
+    return res
+      .status(400)
+      .json({ error: "chatId and numMessages are required" });
+  }
+
+  // Fetch the messages from ChatEngine's API using received credentials
+  const responseCE = await fetch(
+    `https://api.chatengine.io/chats/${chatId}/messages/`,
+    {
+      method: "GET",
+      headers: {
+        "Project-ID": projectID,
+        "User-Name": userName,
+        "User-Secret": userSecret,
+      },
     }
+  );
+  const messages = await responseCE.json();
 
-    const { chatId, numMessages, projectID, userName, userSecret } = req.body;
-    console.log("Received data:", req.body);
+  // Extract last `numMessages` messages, and join them
+  const latestMessages = messages
+    .slice(-numMessages)
+    .map((msg) => msg.text)
+    .join(" ");
+  const chatContent = latestMessages;
 
-    if (!chatId || !numMessages) {
-        return res.status(400).json({ error: "chatId and numMessages are required" });
-    }
+  // Craft the prompt for OpenAI
+  const promptContent = `Summarize the following chat content:\n\n${chatContent}\n`;
 
-    // Fetch the messages from ChatEngine's API using received credentials
-    const responseCE = await fetch(`https://api.chatengine.io/chats/${chatId}/messages/`, {
-        method: 'GET',
-        headers: {
-            'Project-ID': projectID,
-            'User-Name': userName,
-            'User-Secret': userSecret,
-        },
-    });
-    const messages = await responseCE.json();
+  const config = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+  });
 
-    // Extract last `numMessages` messages, and join them
-    const latestMessages = messages.slice(-numMessages).map(msg => msg.text).join(' ');
-    const chatContent = latestMessages;
+  const openai = new OpenAIApi(config);
 
-    // Craft the prompt for OpenAI
-    const promptContent = `Summarize the following chat content:\n\n${chatContent}\n`;
-
-    const config = new Configuration({
-        apiKey: process.env.OPENAI_KEY,
-    });
-
-    const openai = new OpenAIApi(config);
-
+  try {
     const responseOA = await openai.createCompletion({
-        model: 'text-davinci-003',
-        temperature: 0,
-        max_tokens: 8000,
-        prompt: promptContent,
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 8000,
+      prompt: promptContent,
     });
 
-    // Send back the chat ID and the generated summary
     res.status(200).json({
-        chatId: chatId,
-        summary: responseOA.data.choices[0].text.trim()
+      chatId: chatId,
+      summary: responseOA.data.choices[0].text.trim(),
     });
+  } catch (error) {
+    console.error("Error calling OpenAI:", error.message);
+    res.status(500).json({ error: "Error generating summary." });
+  }
 }
